b811651ddea7ad373d5d03a2568458da2cb87649
za7satellite <zeynepaslan_10@hotmail.com>
2019-10-26 12:14:25 +0300

    Add project files.

---
+++ FirstApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // yorum
+            /*
+             * çoklu yorum
+             */
+            /*Console.WriteLine("Hello World");
+            Console.ReadKey();
+            Console.ReadLine();
+            */
+
+
+            Console.WriteLine("Lütfen adınız giriniz :");
+            Console.WriteLine("Giriş yapan kullanıcı:" + Console.ReadLine());
+
+
+            Console.Read();
+
+        }
+    }
+}
